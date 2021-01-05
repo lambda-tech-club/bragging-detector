41cd3d10f740b6d489a82f74ef9f29ce98892856
yoidea <yoidea.cc@gmail.com>
2021-01-05 23:04:20 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -48,18 +48,24 @@ const Home = () => {
     recognizerRef.current.onend = () => {
       setDetecting(false);
     };
-    recognizerRef.current.onresult = event => {
-      [...event.results].slice(event.resultIndex).forEach(result => {
+    recognizerRef.current.onresult = (event) => {
+      [...event.results].slice(event.resultIndex).forEach((result) => {
+        alert(
+          [...event.results]
+            .filter((result) => result.isFinal)
+            .map((result) => result[0].transcript)
+            .join("")
+        );
         const transcript = result[0].transcript;
         if (result.isFinal) {
           // 音声認識が完了して文章が確定
-          setFinalText(prevState => {
+          setFinalText((prevState) => {
             return prevState + transcript;
           });
           setTranscript("");
         } else {
           // 音声認識の途中経過
-          if (tagValues.some(value => transcript.includes(value))) {
+          if (tagValues.some((value) => transcript.includes(value))) {
             // NOTE: ユーザーが効果音を追加しなければデフォルトを鳴らす
             (userMusic || music).play();
             setAlertOpen(true);
@@ -112,7 +118,7 @@ const Home = () => {
               defaultValue={initialTagValues}
               label="反応する単語"
               placeholder="単語を追加 +"
-              onTagChange={values => {
+              onTagChange={(values) => {
                 setTagValues(values);
               }}
             />
@@ -121,7 +127,7 @@ const Home = () => {
             <UploadButton
               disabled={detecting}
               fileType="audio"
-              onFileChange={file => {
+              onFileChange={(file) => {
                 const src = window.URL.createObjectURL(file);
                 const audio = new Audio(src);
                 setUserMusic(audio);
